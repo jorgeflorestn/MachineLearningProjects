643bfde8f157fe22c3b5377c96a0a7ab12c3abf6
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-02 23:50:09 -0600

    Some explanations

--- 04_clustering/media/generate_gif.sh
+++ 04_clustering/media/generate_gif.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-convert -delay 50 -loop 0 iteration-*.png animation.gif
-rm iteration-*.png
+convert -delay 50 -loop 0 04_clustering/media/iteration-*.png 04_clustering/media/animation.gif
+rm 04_clustering/media/iteration-*.png
 
